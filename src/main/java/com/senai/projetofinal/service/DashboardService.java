50036361003d4088a1c40ab848dd039ab11d5348
vitor_schultz <vitorserta@gmail.com>
2024-10-20 16:53:17 -0300

    adiciona get dashboard

---
+++ src/main/java/com/senai/projetofinal/service/DashboardService.java
@@ -0,0 +1,29 @@
+package com.senai.projetofinal.service;
+
+import com.senai.projetofinal.controller.dto.response.dashboard.DashboardResponse;
+import com.senai.projetofinal.datasource.repository.AlunoRepository;
+import com.senai.projetofinal.datasource.repository.DocenteRepository;
+import com.senai.projetofinal.datasource.repository.TurmaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DashboardService {
+
+    private final AlunoRepository alunoRepository;
+    private final DocenteRepository docenteRepository;
+    private final TurmaRepository turmaRepository;
+
+    public DashboardService(AlunoRepository alunoRepository, DocenteRepository docenteRepository, TurmaRepository turmaRepository) {
+        this.alunoRepository = alunoRepository;
+        this.docenteRepository = docenteRepository;
+        this.turmaRepository = turmaRepository;
+    }
+
+    public DashboardResponse getDashboardData() {
+        return new DashboardResponse(
+                alunoRepository.count(),
+                docenteRepository.count(),
+                turmaRepository.count()
+        );
+    }
+}
